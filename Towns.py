d2d1b16e8cf802cc4e240e5311a7989e6d114134
EDTEAGRE\ed <ed@teamgreen.co.uk>
2020-05-13 20:45:47 +0100

    Added some basic road classes

--- Towns.py
+++ Towns.py
@@ -4,8 +4,12 @@ from PIL import Image
 
 
 class Town:
-    def __init__(self, w, h):
-        self.LandWidth, self.LandHeight = w, h
+    def __init__(self, w, h, Entrances, Branches, Density):
+        self.LandWidth = w
+        self.LandHeight = h
+        self.Entrances = Entrances
+        self.Branches = Branches
+        self.Density = Density
 
     def CreateImage(self):
         with Image.new("RGB", (self.LandWidth, self.LandHeight), (66,245,120)) as Img:
@@ -20,5 +24,22 @@ class Town:
 
 
 
-t = Town(100, 100)
+
+class Road:
+    def __init__(self, StartPos, EndPos, w):
+        self.Start = Start
+        self.End = End
+        self.width = w
+
+
+class MainRoad(Road):
+    def __init__(self, StartPos, EndPos, w):
+        super(StartPos, EndPos, w)
+
+class SubRoad(Road):
+    def __init__(self, StartPos, EndPos, w):
+        super(StartPos, EndPos, w)
+
+
+t = Town(250, 200, 2, 10, 3)
 t.CreateImage()
